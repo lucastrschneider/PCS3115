e05616227ca5eee51c7534f3f38a81baf5246cfb
lucastrschneider <lucastrschneider@gmail.com>
2020-07-04 20:21:10 -0300

    Update Makefile and remove old version

---
+++ EP4/description.mk
@@ -0,0 +1,9 @@
+# Name: description.mk
+# Author: Lucas Schneider
+# 07/2020
+
+# Name of all components in priority order
+CPNT_LIST := littlesort_fd
+
+# Name of the component to be tested
+CPNT ?= littlesort_fd
