dbff867a8c4f97a73fb7e0e89fde21ab0fcbcfb4
lucastrschneider <lucastrschneider@gmail.com>
2020-07-10 15:20:46 -0300

    Refactor script

--- EP3/turbo_tb.py
+++ EP3/turbo_tb.py
@@ -1,32 +1,39 @@
-TEST_PER_SENSIB = 100
-WORDS_PER_TEST = 32
+################################################################################
+# @file turbo_tb.py
+# @brief Generate test cases for turbo controller
+# @author Lucas Schneider (lucastrschneider@usp.br)
+# @date 2020-07-10
+################################################################################
+
+import random
+
+TEST_PER_SENSIB = 100  
+WORDS_PER_TEST = 32  # Should not be changed
 
 def print_data(sensib, dataInput, dataOutput):
-    print ("{0:004b}".format(sensib))
+    print (f"{sensib:004b}")
 
     out_input = ""
     for i in range (0, WORDS_PER_TEST):
-        out_input += "{0:008b}".format(dataInput[i]) + " "
+        out_input += f"{dataInput[i]:008b}" + " "
     print(out_input)
     
     out_output = ""
     for i in range (0, WORDS_PER_TEST):
-        out_output += "{0:008b}".format(dataOutput[i]) + " "
+        out_output += f"{dataOutput[i]:008b}" + " "
     print(out_output)
 
-def remap(old_value, old_min, old_max, new_min, new_max):
-    new_value = (float)(old_value - old_min) * (float)(new_max - new_min)
-    new_value /= (float)(old_max - old_min)
-    new_value += (float)(new_min)
+def remap(old_value:float, old_min:float, old_max:float, new_min:float, new_max:float) -> float:
+    new_value = (old_value - old_min) * (new_max - new_min)
+    new_value /= (old_max - old_min)
+    new_value += (new_min)
     return new_value
-
-import random
 
 for i in range(0,4):
     sensib = int(pow(2,i))
     
     for test_number in range (0, TEST_PER_SENSIB):
-        changing_rate = int(remap(test_number, 0, TEST_PER_SENSIB - 1, 0, sensib + 2))
+        changing_rate = int(remap(test_number, 0, TEST_PER_SENSIB - 1, 1, sensib + 2))
         dataInput = []
         dataOutput = []
         value = random.randint(0, 255)
@@ -56,4 +63,4 @@ for i in range(0,4):
 
             dataInput.append(value)
         
-        print_data(sensib, dataInput, dataOutput);
+        print_data(sensib, dataInput, dataOutput)
